559c755a5af49178e7c45f43c10dcd2fe95d5513
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2019-04-06 10:59:31 +0200

    Adding Test Data for integration testing (#4)

--- src/test/java/at/madlmayr/ToolCallRequest2DynamoDbTest.java
+++ src/test/java/at/madlmayr/ToolCallRequest2DynamoDbTest.java
@@ -15,8 +15,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,7 +25,8 @@ public class ToolCallRequest2DynamoDbTest {
 
     private static final Logger LOGGER = LogManager.getLogger(ToolCallRequest2DynamoDbTest.class);
     private static DynamoDBProxyServer server;
-
+    private static final String FAKE_COMPANY_1 = "gma";
+    private static final String FAKE_COMPANY_2 = "another";
 
     @BeforeAll
     public static void beforeAll() throws Exception {
@@ -64,13 +66,14 @@ public class ToolCallRequest2DynamoDbTest {
         LOGGER.info("Table '{}' created", result.getTableDescription().getTableName());
     }
 
-    private static void insertData() throws IOException {
+    private static void insertData() {
         AmazonDynamoDB ddb = getConnectionLocalhost();
         DynamoDBMapper mapper = new DynamoDBMapper(ddb);
 
-        for (ToolCallRequest obj : CallUtils.readToolConfigFromCVSFile()) {
-            mapper.save(obj);
-        }
+        mapper.save(new ToolCallRequest(new String[]{FAKE_COMPANY_1, ToolEnum.SLACK.getName(), "somekey", "http://localhost:8080/api/users.list/"}, 1L));
+        mapper.save(new ToolCallRequest(new String[]{FAKE_COMPANY_1, ToolEnum.ARTIFACTORY.getName(), "somekey", "http://localhost:8080/gma/api/security/users"}, 1L));
+        mapper.save(new ToolCallRequest(new String[]{FAKE_COMPANY_1, ToolEnum.JIRA.getName(), "somekey", "http://localhost:8080/rest/api/2/user/search"}, 1L));
+        mapper.save(new ToolCallRequest(new String[]{FAKE_COMPANY_2, ToolEnum.JIRA.getName(), "somekey", "http://localhost:8080/rest/api/2/user/search"}, 1L));
     }
 
     private static void deleteTable() {
@@ -85,15 +88,25 @@ public class ToolCallRequest2DynamoDbTest {
         DynamoDBMapper mapper = new DynamoDBMapper(ddb);
 
         ToolCallRequest query = new ToolCallRequest();
-        query.setCompany("gma");
+        query.setCompany(FAKE_COMPANY_1);
 
         DynamoDBQueryExpression<ToolCallRequest> queryExpression = new DynamoDBQueryExpression<ToolCallRequest>()
                 .withHashKeyValues(query);
 
         List<ToolCallRequest> itemList = mapper.query(ToolCallRequest.class, queryExpression);
-        LOGGER.info("Amount of Element '{}'", itemList.size());
+
         assertThat(itemList.size() == 3);
+
+        Set<String> toolNames = new HashSet<>();
+        for (ToolCallRequest call : itemList) {
+            toolNames.add(call.getTool());
+        }
+
+        assertThat(toolNames.contains(ToolEnum.SLACK.getName()));
+        assertThat(toolNames.contains(ToolEnum.JIRA.getName()));
+        assertThat(toolNames.contains(ToolEnum.ARTIFACTORY.getName()));
     }
 
 
 }
+
