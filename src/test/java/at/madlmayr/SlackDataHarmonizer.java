104a6dc1ad9e25cc2c2ed5cf5daf9f423452fa21
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2019-04-10 18:45:47 +0200

    Working no Data Harmonizer for Slack

--- src/test/java/at/madlmayr/SlackDataHarmonizer.java
+++ src/test/java/at/madlmayr/SlackDataHarmonizer.java
@@ -7,8 +7,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class SlackDataHarmonizer {
@@ -27,12 +30,12 @@ public class SlackDataHarmonizer {
         }
 
         SlackResponse response = objectMapper.readValue(buffer.toString(), SlackResponse.class);
-        String generatedTeamId = getRandomHexString(9).toUpperCase();
+        String generatedTeamId = "T" + getRandomHexString(8).toUpperCase();
 
         for (SlackMember member : response.getMembers()) {
             member.setTeamId(generatedTeamId);
             member.getProfile().setTeam(generatedTeamId);
-            member.setId(getRandomHexString(9).toUpperCase());
+            member.setId("U" + getRandomHexString(8).toUpperCase());
             member.getProfile().setAvatarHash(getRandomHexString(12).toLowerCase());
             member.getProfile().setImage24(new URL("https://i1.wp.com/a.slack-edge.com/00b63/img/avatars/ava_0007-24.png"));
             member.getProfile().setImage32(new URL("https://i1.wp.com/a.slack-edge.com/00b63/img/avatars/ava_0007-32.png"));
@@ -42,7 +45,6 @@ public class SlackDataHarmonizer {
             member.getProfile().setImage512(new URL("https://i1.wp.com/a.slack-edge.com/00b63/img/avatars/ava_0007-512.png"));
             member.getProfile().setImage1024(new URL("https://i1.wp.com/a.slack-edge.com/00b63/img/avatars/ava_0007-1024.png"));
         }
-
     }
 
     // taken from: https://stackoverflow.com/questions/14622622/generating-a-random-hex-string-of-length-50-in-java-me-j2me
@@ -52,8 +54,18 @@ public class SlackDataHarmonizer {
         while (sb.length() < numchars) {
             sb.append(Integer.toHexString(r.nextInt()));
         }
-
         return sb.toString().substring(0, numchars);
+    }
+
+    private List<String> readNames() throws IOException {
+        List<String> names = new ArrayList();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(CallUtils.class.getResourceAsStream("/slackdata_01.raw")))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                names.add(line);
+            }
+        }
+        return  names;
     }
 
 }
