368bba9a69f5599367089284e22c50cf88998d3a
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2019-04-15 21:57:13 +0200

    Adding current surefire plugin for testcoverage (#13)

--- src/main/java/at/madlmayr/Utils.java
+++ src/main/java/at/madlmayr/Utils.java
@@ -6,19 +6,19 @@ import java.util.TimeZone;
 
 public class Utils {
 
-    private final static String pattern = "yyyy-MM-dd_HH:mm:ss.SSS";
-    private final static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-
-    static {
-        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Vienna"));
-    }
+    private final static String PATTERN = "yyyy-MM-dd_HH:mm:ss.SSS";
+    private final static String TIME_ZONE = "Europe/Vienna";
 
     public static String standardTimeFormat(final long time) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
         return simpleDateFormat.format(time);
     }
 
     public static long parseStandardTime(final String timestamp) {
         try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
+            simpleDateFormat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
             return simpleDateFormat.parse(timestamp).getTime();
         } catch (ParseException e) {
             throw new ToolCallException(e);
